d37f41eb19fb997435d723ebf178aa5131be51fd
cihatislamdede <chtddgs@hotmail.com>
2022-08-04 14:22:45 +0300

    add return types

--- algocid.py
+++ algocid.py
@@ -1,5 +1,5 @@
 import base64
-
+from ast import literal_eval
 import requests
 from algosdk import v2client
 from endpoints.indexer_endpoints import ALGONODE_INDEXER_MAINNET, ALGONODE_INDEXER_TESTNET
@@ -30,29 +30,29 @@ class Account:
         self.algod_client = algod_client
         self.indexer_client = indexer_client
 
-    def info(self, address: str):
+    def info(self, address: str) -> dict:
         return self.indexer_client.account_info(address)
 
-    def round(self):
+    def round(self) -> int:
         return self.indexer_client.health()["round"]
 
-    def balance(self, address: str, readable: bool = False):
+    def balance(self, address: str, readable: bool = False) -> float or int:
         balance = self.indexer_client.account_info(address)["account"]["amount"]
         if readable:
             return balance / 10**6
         return balance
 
-    def created_asset_ids(self, account_address: str):
+    def created_asset_ids(self, account_address: str) -> list:
         account_info = self.algod_client.account_info(account_address)
         created_asset_ids = [asset["index"] for asset in account_info["created-assets"]]
         return created_asset_ids
 
-    def asset_ids(self, account_address: str):
+    def asset_ids(self, account_address: str) -> list:
         account_info = self.algod_client.account_info(account_address)
         asset_ids = [asset["asset-id"] for asset in account_info["assets"]]
         return asset_ids
 
-    def is_holder(self, account_address: str, creator_addresses: list):
+    def is_holder(self, account_address: str, creator_addresses: list) -> bool:
         creator_assets = []
         for creator_address in creator_addresses:
             creator_assets += self.created_asset_ids(creator_address)
@@ -66,33 +66,33 @@ class Asset:
         self.algod_client = algod_client
         self.indexer_client = indexer_client
 
-    def info(self, asset_id: str):
+    def info(self, asset_id: str) -> dict:
         return self.indexer_client.asset_info(asset_id)
 
-    def round(self):
+    def round(self) -> int:
         return self.indexer_client.health()["round"]
 
-    def params(self, asset_id: str):
+    def params(self, asset_id: str) -> dict:
         return self.indexer_client.asset_info(asset_id)["asset"]["params"]
 
-    def name(self, asset_id: str):
+    def name(self, asset_id: str) -> str:
         return self.params(asset_id)["name"]
 
-    def unit_name(self, asset_id: str):
+    def unit_name(self, asset_id: str) -> str:
         return self.params(asset_id)["unit-name"]
 
-    def url(self, asset_id: str):
+    def url(self, asset_id: str) -> str:
         return self.params(asset_id)["url"]
 
-    def creator(self, asset_id: str):
+    def creator(self, asset_id: str) -> str:
         return self.params(asset_id)["creator"]
 
-    def metadata(self, asset_id: str):
+    def metadata(self, asset_id: str) -> dict:
         req = requests.get(f"https://mainnet-idx.algonode.cloud/v2/assets/{asset_id}/transactions?tx-type=acfg").json()["transactions"]
         note = req[len(req) - 1]["note"]
-        return base64.b64decode(note).decode("utf-8")
+        return literal_eval(base64.b64decode(note).decode("utf-8"))
 
-    def holders(self, asset_id: str, only_wallets: bool = False):
+    def holders(self, asset_id: str, only_wallets: bool = False) -> list:
         if not only_wallets:
             return requests.get(f"https://mainnet-idx.algonode.cloud/v2/assets/{asset_id}/balances?currency-greater-than=0").json()["balances"]
         else:
