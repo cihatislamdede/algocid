9050ed58f3dcc2c8cebbe73ce003864bb829b9ef
cihatislamdede <chtddgs@hotmail.com>
2022-08-10 13:01:23 +0300

    add TransactionFetcher and docstrings

--- algocid.py
+++ algocid.py
@@ -1,8 +1,12 @@
 import base64
 from ast import literal_eval
+
 import requests
 from algosdk import v2client
-from constants import ALGONODE_INDEXER_MAINNET, ALGONODE_INDEXER_TESTNET, ALGONODE_NODE_MAINNET, ALGONODE_NODE_TESTNET, IS_TESTNET
+
+from constants import (ALGONODE_INDEXER_MAINNET, ALGONODE_INDEXER_TESTNET,
+                       ALGONODE_NODE_MAINNET, ALGONODE_NODE_TESTNET,
+                       IS_TESTNET)
 
 
 def get_v2_algod_client(is_tesnet=IS_TESTNET):
@@ -33,28 +37,47 @@ class AccountFetcher:
         self.indexer_client = indexer_client
 
     def info(self, address: str) -> dict:
+        '''
+        Returns the account info.
+        '''
         return self.indexer_client.account_info(address)
 
     def round(self) -> int:
+        '''
+        Returns the current round.
+        '''
         return self.indexer_client.health()["round"]
 
     def balance(self, address: str, readable: bool = False) -> float or int:
+        '''
+        Returns the balance of the account.\n
+        readable: If True, returns the balance in readable format. (default: False, returns the balance in microAlgos format.)
+        '''
         balance = self.indexer_client.account_info(address)["account"]["amount"]
         if readable:
             return balance / 10**6
         return balance
 
     def created_asset_ids(self, account_address: str) -> list:
+        '''
+        Returns the asset ids of the assets created by the account.
+        '''
         account_info = self.algod_client.account_info(account_address)
         created_asset_ids = [asset["index"] for asset in account_info["created-assets"]]
         return created_asset_ids
 
     def asset_ids(self, account_address: str) -> list:
+        '''
+        Returns the asset ids of the assets the account has.
+        '''
         account_info = self.algod_client.account_info(account_address)
         asset_ids = [asset["asset-id"] for asset in account_info["assets"]]
         return asset_ids
 
     def is_holder(self, account_address: str, creator_addresses: list) -> bool:
+        '''
+        Check if the account is a holder of any of the assets created by the creator addresses.
+        '''
         creator_assets = []
         for creator_address in creator_addresses:
             creator_assets += self.created_asset_ids(creator_address)
@@ -69,21 +92,39 @@ class AssetFetcher:
         self.indexer_client = indexer_client
 
     def info(self, asset_id: str) -> dict:
+        '''
+        Returns the asset info.
+        '''
         return self.indexer_client.asset_info(asset_id)
 
     def round(self) -> int:
+        '''
+        Returns the current round.
+        '''
         return self.indexer_client.health()["round"]
 
     def params(self, asset_id: str) -> dict:
+        '''
+        Returns the asset params.
+        '''
         return self.indexer_client.asset_info(asset_id)["asset"]["params"]
 
     def name(self, asset_id: str) -> str:
+        '''
+        Returns the asset name.
+        '''
         return self.params(asset_id)["name"]
 
     def unit_name(self, asset_id: str) -> str:
+        '''
+        Returns the asset unit-name.
+        '''
         return self.params(asset_id)["unit-name"]
 
     def url(self, asset_id: str) -> str:
+        '''
+        Returns the asset url.
+        '''
         try:
             return self.metadata(asset_id)["url"]
         except KeyError:
@@ -91,9 +132,15 @@ class AssetFetcher:
             return ""
 
     def creator(self, asset_id: str) -> str:
+        '''
+        Returns the asset creator.
+        '''
         return self.params(asset_id)["creator"]
 
     def metadata(self, asset_id: str) -> dict:
+        '''
+        Returns the asset metadata.
+        '''
         try:
             req = requests.get(f"{indexer_request_url}/assets/{asset_id}/transactions?tx-type=acfg").json()["transactions"]
             note = req[len(req) - 1]["note"]
@@ -106,8 +153,24 @@ class AssetFetcher:
             return {}
 
     def holders(self, asset_id: str, only_wallets: bool = False) -> list:
+        '''
+        Returns the asset holders.\n
+        only_wallets: If True, only returns the wallets that hold the asset. (default: False)
+        '''
         if not only_wallets:
             return requests.get(f"{indexer_request_url}/assets/{asset_id}/balances?currency-greater-than=0").json()["balances"]
         else:
             holders = requests.get(f"{indexer_request_url}/assets/{asset_id}/balances?currency-greater-than=0").json()["balances"]
             return [holder["address"] for holder in holders]
+
+
+class TransactionFetcher:
+    def __init__(self, algod_client: v2client.algod.AlgodClient, indexer_client: v2client.indexer.IndexerClient):
+        self.algod_client = algod_client
+        self.indexer_client = indexer_client
+
+    def info(self, transaction_id: str) -> dict:
+        '''
+        Returns the transaction info.
+        '''
+        return self.indexer_client.transaction(transaction_id)["transaction"]
--- examples.py
+++ examples.py
@@ -1,37 +1,44 @@
-from algocid import AccountFetcher, AssetFetcher, get_v2_algod_client, get_v2_indexer_client
+from algocid import (AccountFetcher, AssetFetcher, TransactionFetcher,
+                     get_v2_algod_client, get_v2_indexer_client)
 from constants import IS_TESTNET
 
-
-account = AccountFetcher(indexer_client=get_v2_indexer_client(is_tesnet=IS_TESTNET), algod_client=get_v2_algod_client(is_tesnet=IS_TESTNET))
-asset = AssetFetcher(indexer_client=get_v2_indexer_client(is_tesnet=IS_TESTNET), algod_client=get_v2_algod_client(is_tesnet=IS_TESTNET))
+account_fetcher = AccountFetcher(indexer_client=get_v2_indexer_client(is_tesnet=IS_TESTNET), algod_client=get_v2_algod_client(is_tesnet=IS_TESTNET))
+asset_fetcher = AssetFetcher(indexer_client=get_v2_indexer_client(is_tesnet=IS_TESTNET), algod_client=get_v2_algod_client(is_tesnet=IS_TESTNET))
+transaction_fetcher = TransactionFetcher(indexer_client=get_v2_indexer_client(is_tesnet=IS_TESTNET), algod_client=get_v2_algod_client(is_tesnet=IS_TESTNET))
 
 if IS_TESTNET:
     address = "VETIGP3I6RCUVLVYNDW5UA2OJMXB5WP6L6HJ3RWO2R37GP4AVETICXC55I"
     asset_id = "10458941"
+    transaction_id = "PU6ZNPD563QETUFMGLFCWN4OKBZQKXGY3E64DYEWUY23KKZ5TJ7A"
 else:
-    address = "GLOY2UAW3EVUBFMW6XGKUPZITLWPOGIJTEWPADZOIKHTGJLL4GNQUYED4M"
-    asset_id = "719923979"
+    address = "D5J7H7PIYKLY2U6A5OFUAC7GQHTHSXXNX65DSD3CJYPBV2MVK6NTNW44CA"
+    asset_id = "326189642"
+    transaction_id = "VNYJRN44VLJ5VSFLJRQQNC4PG5FDQOL2BYAWIIKVZIEEJFUJLKZA"
 
 print("##############################")
 print("address:", address)
 print("asset_id:", asset_id)
 print("##############################")
-# Account examples
-# print(account.info(address))
-# print("balance:", account.balance(address, readable=True))
-# print(account.balance(address))
-# print("created asset ids", account.created_asset_ids(address))
-# print("asset ids:", account.asset_ids(address))
+# AccountFetcher examples
+# print(account_fetcher.info(address))
+# print("balance:", account_fetcher.balance(address, readable=True))
+# print(account_fetcher.balance(address))
+# print("created asset_fetcher ids", account_fetcher.created_asset_ids(address))
+# print("asset_fetcher ids:", account_fetcher.asset_ids(address))
 
 
-# # Asset examples
-# print(asset.info(asset_id=asset_id))
-# print("asset creator:", asset.creator(asset_id=asset_id))
-# print(asset.creator(asset_id))
-# print(asset.name(asset_id))
-# print(asset.unit_name(asset_id))
-# print(asset.url(asset_id))
-# print(asset.params(asset_id))
-# print(asset.metadata(326189642))
-print(asset.holders(asset_id))
-print(asset.holders(asset_id, only_wallets=True))
+# AssetFetcher examples
+# print(asset_fetcher.info(asset_id=asset_id))
+# print("asset_fetcher creator:", asset_fetcher.creator(asset_id=asset_id))
+# print(asset_fetcher.creator(asset_id))
+# print(asset_fetcher.name(asset_id))
+# print(asset_fetcher.unit_name(asset_id))
+# print(asset_fetcher.url(asset_id))
+# print(asset_fetcher.params(asset_id))
+# print(asset_fetcher.metadata(326189642))
+# print(asset_fetcher.holders(asset_id))
+# print(asset_fetcher.holders(asset_id, only_wallets=True))
+
+
+# TransactionFetcher examples
+# print(transaction_fetcher.info(transaction_id=transaction_id))
